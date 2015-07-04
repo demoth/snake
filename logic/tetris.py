f493663c7b605bdc012aaa0eeeb50d790dd35b74
demoth <demoth.cadaver@gmail.com>
2015-07-04 15:44:58 +0300

    beta version of tetris
     * no score
     * no rotation
     * no down control

--- logic/tetris.py
+++ logic/tetris.py
@@ -1,20 +1,37 @@
-from logic import Game
-import string
 import random
+
+from logic import Game
+
+BLOCK = '█'
 
 class TetrisGame(Game):
     """ (author: demoth)
     """
+    WIDTH = 10
+    HEIGHT = 16
 
-    WIDTH = 30
-    HEIGHT = 30
+    RATE = 333
 
-    RATE = 1
+    SHAPES = [
+        [[1, 1], [1, 1]],
+        [[1, 1, 1, 1]],
+        [[1, 1, 1], [0, 1, 0]],
+        [[1, 1, 0], [0, 1, 1]],
+        [[0, 1, 1], [1, 1, 0]],
+        [[1, 0, 0], [1, 1, 1]],
+        [[0, 0, 1], [1, 1, 1]]
+    ]
+
+    def __init__(self, screen):
+        super().__init__(screen)
+        self.figure = self.createNewFigure()
+        self.landed = [[0 for x in range(self.WIDTH)] for y in range(self.HEIGHT)]
+        self.sideControl = 0
 
     def update(self):
-        x = random.randint(0, self.WIDTH - 1)
-        y = random.randint(0, self.HEIGHT - 1)
-        self.screen[(x, y)] = random.choice(string.punctuation)
+        self.draw()
+        self.update1()
+        pass
 
     def up(self, event):
         pass
@@ -23,7 +40,71 @@ class TetrisGame(Game):
         pass
 
     def left(self, event):
-        pass
+        self.sideControl = -1
 
     def right(self, event):
-        pass
+        self.sideControl = 1
+
+    def update1(self):
+        if self.collideWithBottomAndLanded():
+            self.landed = [[0 for x in range(self.WIDTH)] for y in range(self.HEIGHT)]
+            self.figure = self.createNewFigure()
+
+        self.figure['left'] += self.sideControl
+        if self.collideWithWallsAndLanded():
+            self.figure['left'] -= self.sideControl
+        self.sideControl = 0
+
+        self.figure['top'] += 1
+        if self.collideWithBottomAndLanded():
+            self.figure['top'] -= 1
+            self.landFigure()
+            self.clearLinesAndShift()
+            self.figure = self.createNewFigure()
+
+    def clearLinesAndShift(self):
+        for rowIndex, row in enumerate(self.landed):
+            fullrow = True
+            for colIndex, col in enumerate(row):
+                if col == 0:
+                    fullrow = False
+            if fullrow:
+                for ci in range(0, self.WIDTH):
+                    for ri in range(rowIndex, 0, -1):
+                        self.landed[ri][ci] = self.landed[ri - 1][ci]
+
+    def createNewFigure(self):
+        return {'top': 0, 'left': self.WIDTH // 2 - 1, 'shape': random.choice(self.SHAPES)}
+
+    def draw(self):
+        self.screen.clear()
+        # draw landed
+        for rowIndex, row in enumerate(self.landed):
+            for colIndex, col in enumerate(row):
+                if col == 1:
+                    self.screen[(colIndex, rowIndex)] = BLOCK
+        # draw figure
+        for rowIndex, row in enumerate(self.figure['shape']):
+            for colIndex, col in enumerate(row):
+                if col == 1:
+                    self.screen[(self.figure['left'] + colIndex, self.figure['top'] + rowIndex)] = BLOCK
+
+    def collideWithBottomAndLanded(self):
+        for rowIndex, row in enumerate(self.figure['shape']):
+            for colIndex, col in enumerate(row):
+                if col == 1 and (self.figure['top'] + rowIndex == self.HEIGHT
+                                 or self.landed[self.figure['top'] + rowIndex][self.figure['left'] + colIndex] == 1):
+                    return True
+
+    def landFigure(self):
+        for rowIndex, row in enumerate(self.figure['shape']):
+            for colIndex, col in enumerate(row):
+                if col == 1:
+                    self.landed[self.figure['top'] + rowIndex][self.figure['left'] + colIndex] = 1
+
+    def collideWithWallsAndLanded(self):
+        for rowIndex, row in enumerate(self.figure['shape']):
+            for colIndex, col in enumerate(row):
+                if col == 1 and (self.figure['left'] + colIndex < 0 or self.figure['left'] + colIndex > self.WIDTH - 1
+                                 or self.landed[self.figure['top'] + rowIndex][self.figure['left'] + colIndex] == 1):
+                    return True
